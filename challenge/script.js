8498a2f487e8eda7fec4e9b6e092ba9c4fd835f4
Lambda <alexe@Alexs-MacBook-Pro-2.local>
2019-10-31 03:13:33 +0000

    .

--- challenge/script.js
+++ challenge/script.js
@@ -1,24 +1,3 @@
-const cardData = [
-  {
-    title: "Octavius Kraken",
-    subtitle: "Escape Artist",
-    content: "Super Powers: can shape-shift, instantly change color, communicate using light displays generated on his skin, taste things by touching them, squirt ink and use tools",
-    imgsrc: "https://images.unsplash.com/photo-1564261331932-ea97c182f705?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-  },
-  {
-    title: "Leonardo ",
-    subtitle: "Surfer / Pizza lover",
-    content: "Super Powers: Decelerated Aging, Dermal Armor/Shell Formation, Enhanced Durability, Enhanced Endurance, Retractable Head, Night Vision, Speed Swimming.",
-    imgsrc: "https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-  },
-  {
-    title: "Captain Nemo",
-    subtitle: "Comedian",
-    content: "Super Powers: Live in a sea anenome away from predators. Has a slimy mucus covering his body that protects him from the sea anemone. Tells good dad jokes.",
-    imgsrc: "https://images.unsplash.com/photo-1536168032936-9ce3b4b3165c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-  },
-];
-
 function createCard(title, imgsrc) {
   const card = document.createElement('div');
   const image = document.createElement('img');
