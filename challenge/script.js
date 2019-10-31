06cab569bfe7242eac1f949c0a9edb830e5f84d3
AlexEntrepreneur <alexe@Alexs-MacBook-Pro-2.local>
2019-10-31 03:15:31 +0000

    .

--- challenge/script.js
+++ challenge/script.js
@@ -2,31 +2,19 @@ function createCard(title, imgsrc) {
   const card = document.createElement('div');
   const image = document.createElement('img');
   const name = document.createElement('h2');
-  // const sub = document.createElement('p');
-  // const description = document.createElement('p');
 
   card.append(image);
   card.append(name);
-  // card.append(sub);
-  // card.append(description);
 
   card.classList.add('card');
-  // sub.classList.add('subtitle');
-  // description.classList.add('desc');
 
   name.textContent = title;
   image.src = imgsrc;
-  // sub.textContent = subtitle;
-  // description.textContent = content;
 
   return card;
 }
 
 const container = document.querySelector('.container');
-
-// cardData.forEach((card => {
-//   container.append(createCard(card.title, card.imgsrc));
-// }));
 
 axios.get('https://dog.ceo/api/breeds/image/random/12')
   .then(response => {
